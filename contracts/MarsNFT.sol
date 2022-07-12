5535060b42fbff0ba2e1722fe24a0ecb2254d8b8
Anne Chretien <chretien.an@gmail.com>
2022-07-12 17:53:17 +0200

    add NFT contract

---
+++ contracts/MarsNFT.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+//This contract is going to allow users to mint an NFT representing 100sqm
+// of a plot of land on Mars. 
+//There are only 10 unique NFTs available which price is set. First arrived,first served
+//The minter won't know which plot they will get, but they will be told how many are left for sale
+
+
+// import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// import "../node_modules/@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+// import "../node_modules/@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+// import "hardhat/console.sol";
+
+
+contract MarsNFT {
+//When we mint an NFT, we will trigger a Chainlink VRF Call to get a random number
+//This random number will be determining which
+
+
+}
