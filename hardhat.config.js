07573300c779e4ee5ad6da04765f1bb9ff3a6659
Anne Chretien <chretien.an@gmail.com>
2022-07-13 11:13:11 +0200

    add dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
 
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
-require("./tasks/faucet");
+require("./tasks/faucet")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
