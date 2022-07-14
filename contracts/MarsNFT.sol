ec6cd8728c1ec450a0b0469b395cf3102839fccd
Anne Chretien <chretien.an@gmail.com>
2022-07-14 19:12:22 +0200

    add chainlink

--- contracts/MarsNFT.sol
+++ contracts/MarsNFT.sol
@@ -19,32 +19,54 @@ import "../node_modules/@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV
 
 
 
-contract MarsNFT is ERC721, Ownable {
+contract MarsNFT is ERC721, Ownable, VRFConsumerBaseV2 {
 
+
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    // VRF Helpers
+    mapping(uint256 => address) public s_requestIdToSender;
+
+   // NFT Variables
+    uint256 private nftFee;//price expected to be paid to mint one NFT Plot On Mars
+    uint256 public s_tokenCounter;
+    uint256 internal constant MAX_CHANCE_VALUE = 100;
+    string[] internal s_plotTokenUris;
+    bool private s_initialized;
     //Contract's variables
     uint256 private _tokenId; //the unique tokenID of the NFT deployed
-    uint256 private nftFee;//price expected to be paid to mint one NFT Plot On Mars
-
     mapping(address => uint256) private _ownerToId; //link each NFT to their owner
-
     error NeedMoreEth(); //throws if the address minting the NFT does not send the correct nftFee
-
     event NftRequested(uint256 indexed requestId, address requester);//will emit to inform of the request
     //to get a random number, will call fulfillRandomWords from VRF
     
-    constructor() ERC721("PlotOnMars", "POM"){
+
+    constructor(
+        address vrfCoordinatorV2,
+        uint64 subscriptionId,
+        bytes32 gasLane, // keyHash
+        uint256 mintFee,
+        uint32 callbackGasLimit) 
+        VRFConsumerBaseV2(vrfCoordinatorV2)
+        ERC721("PlotOnMars", "POM"){
+            i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+            i_gasLane = gasLane;
+            i_subscriptionId = subscriptionId;
+            nftFee = mintFee;
+            i_callbackGasLimit = callbackGasLimit;
     }
-
-    function withdraw() public onlyOwner {
-    } //only the owner of the contract can withdraw the ETH paid for the NFTs
-
-
-
 
     // function requestNft() public returns (uint256 requestId) {}
     // // from VRFCoordinator, returns the requestID to fulfill random Words
 
-    // function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {}
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {}
 
-    // function tokenURI(uint256) public{}
+    function withdraw() public onlyOwner {
+    } //only the owner of the contract can withdraw the ETH paid for the NFTs
+ 
 }
