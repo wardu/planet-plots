d9552a16ecc71ab4bf75eeb374452e756a851dd2
Anne Chretien <chretien.an@gmail.com>
2022-07-14 14:10:08 +0200

    test set token name&symbol

--- test/Mars.js
+++ test/Mars.js
@@ -18,12 +18,19 @@ describe("Plot On Mars contract", function () {
 
 
     describe("Deployment", function () {
-      
+        let _name='PlotOnMars';
+        let _symbol='POM';
         it("Should set the right owner", async function () {
           // We use loadFixture to setup our environment, get the deployer's address
           // and get the 
           const { marsToken, owner } = await loadFixture(deployTokenFixture);
           expect(await marsToken.owner()).to.equal(owner.address);
-        });
-    }) // passed correctly 14.07.22 - 13h52
+        }); // passed correctly 14.07.22 - 13h52
+
+        it("Should have the correct name and symbol", async function (){
+          const { marsToken } = await loadFixture(deployTokenFixture);
+          expect(await marsToken.name()).to.equal(_name);
+          expect(await marsToken.symbol()).to.equal(_symbol);
+        });// passed correctly 14.07.22 - 14h09
+    }) 
 })
