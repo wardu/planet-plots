bb3580a56f5b817d2c0593bea7238c952732138a
Wardu <wardu@wardu.home>
2022-07-14 10:12:57 +0100

    Change Metamask button

--- frontend/src/components/ConnectWallet.js
+++ frontend/src/components/ConnectWallet.js
@@ -1,25 +1,22 @@
-import React from "react";
+import React from 'react';
 
-import { NetworkErrorMessage } from "./NetworkErrorMessage";
+import { NetworkErrorMessage } from './NetworkErrorMessage';
 
 export function ConnectWallet({ connectWallet, networkError, dismiss }) {
   return (
-    <div className="container">
-      <div className="row justify-content-md-center">
-        <div className="col-12 text-center">
+    <div className='container'>
+      <div className='row justify-content-md-center'>
+        <div className='col-12 text-center'>
           {/* Metamask network should be set to Localhost:8545. */}
           {networkError && (
-            <NetworkErrorMessage 
-              message={networkError} 
-              dismiss={dismiss} 
-            />
+            <NetworkErrorMessage message={networkError} dismiss={dismiss} />
           )}
         </div>
-        <div className="col-6 p-4 text-center">
-          <p>Please connect to your wallet.</p>
+        <div className='col-6 p-4 text-center'>
+          <p>Please connect to Metamask</p>
           <button
-            className="btn btn-warning"
-            type="button"
+            className='btn btn-info btn-lg'
+            type='button'
             onClick={connectWallet}
           >
             Connect Wallet
