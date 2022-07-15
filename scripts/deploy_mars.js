e6486f7efd0f2a1d4bc8c61f76ad2809b10aea4c
Anne Chretien <chretien.an@gmail.com>
2022-07-15 15:07:17 +0200

    requestNFT + desc fulfill

--- scripts/deploy_mars.js
+++ scripts/deploy_mars.js
@@ -1,6 +1,8 @@
 //const path = require("path")
 
 
+vrfCoordinatorV2 =
+
 async function main() {
     // first, we get the address who will deploy the contract
 
@@ -13,10 +15,20 @@ async function main() {
     //then we deploy the contract
     const Mars = await ethers.getContractFactory("MarsNFT");
     const marsToken = await Mars.deploy();
+
+    address vrfCoordinatorV2,
+    uint64 subscriptionId,
+    bytes32 gasLane, // keyHash
+    uint256 mintFee,
+    uint32 callbackGasLimit) 
+// need to add the parameters into the constructor above in the deploy()
+
     await marsToken.deployed();
 
     console.log("Our Plot On Mars NFT address is:", marsToken.address);
     console.log("Deployer's account balance:", (await deployer.getBalance()).toString());
+
+    
 
 }
 
