895568aa4fd4eb0c887c289680fc41cddaf9c2fc
Anne Chretien <chretien.an@gmail.com>
2022-07-16 12:00:36 +0200

    added verification plugin adn etherscan api key

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
+require("hardhat-etherscan")
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
