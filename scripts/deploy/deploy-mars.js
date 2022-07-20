2b91f0b2cbe0cfe62c71d2b0689023e4a2f6c4b7
Anne Chretien <chretien.an@gmail.com>
2022-07-20 16:24:12 +0200

    test deploy fail

---
+++ scripts/deploy/deploy-mars.js
@@ -0,0 +1,81 @@
+//const path = require("path")
+
+const { network, ethers } = require("hardhat")
+const verify = require("../utils/verify")
+require("dotenv").config()
+const { networkConfig } = require("../../helper-hardhat-config")
+
+
+const BASE_FEE = ethers.utils.parseEther("0.25") // premium fee per request
+const GAS_PRICE_LINK = 1e9 // 1000000000 link per gas, calculated based on the value of gas
+const FUND_AMOUNT = "1000000000000000000000"
+
+async function main() {
+    // first, we get the address who will deploy the contract
+
+    const [deployer] = await ethers.getSigners()
+    const chainId = network.config.chainId
+    console.log(chainId)
+
+    console.log("Deploying the contracts with the account:", await deployer.getAddress())
+
+    // then get the mock if we are on hardhat, otherwise get the subscriptionId and contract's address from
+    if (network.name === "hardhat") {
+        console.warn("You are using hardhat's network, you need Mocks. Deploying them for you...");
+        const VRF = await ethers.getContractFactory("VRFCoordinatorV2Mock");
+        const MockCoordinator = await VRF.deploy(BASE_FEE,GAS_PRICE_LINK);
+        await MockCoordinator.deployed();
+        console.log("Mocks Deployed!");
+        console.log("---------------------------------------------");
+        console.log("You are deploying to a local network, you'll need a local network running to interact");
+        console.log("Please run `npx hardhat console --network localhost` to interact with the deployed smart contracts!");
+        console.log("---------------------------------------------");
+        
+        vrfCoordinatorV2Address = MockCoordinator.address
+        console.log(vrfCoordinatorV2Address)
+
+
+        const tx = await MockCoordinator.createSubscription()
+        const txReceipt = await tx.wait(1)
+        subscriptionId = txReceipt.events[0].args.subId
+        await MockCoordinator.fundSubscription(subscriptionId, FUND_AMOUNT)
+      } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+        subscriptionId = networkConfig[chainId].subscriptionId
+    }
+
+    const args = [
+        vrfCoordinatorV2Address, 
+        subscriptionId, 
+        networkConfig.name.gasLane, 
+        networkConfig.name.mintFee,
+        networkConfig.name.chainId.callbackGasLimit,
+    ]
+
+    //then we deploy the contract
+    const marsFactory = await ethers.getContractFactory("MarsNFT")
+    const marsToken = await marsFactory.deploy("RandomIpfsNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,})
+
+    await marsToken.deployed()
+
+    console.log("Our Plot On Mars NFT address is:", marsToken.address)
+    console.log("Deployer's account balance is now:", (await deployer.getBalance()).toString())
+
+    console.log(network.config)
+    if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+        await marsToken.deployTransaction.wait(6) //make sure we give etherscan the time to process a tx
+        await verify(marsToken.address, [])
+    }
+    console.log("Your contract has been verified on etherscan")
+}
+//return the asynchronous main function, throws error if problem
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
